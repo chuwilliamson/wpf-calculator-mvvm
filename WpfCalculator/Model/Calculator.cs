a6228ed2d84fee63849b0a3aa760bfc7b772cc95
chuwilliamson <chuwilliamson@gmail.com>
2024-09-24 13:26:44 -0500

    add calculator model to handle business logic. the logic will be addition subtraction multiplication and division

---
+++ WpfCalculator/Model/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCalculator.Model
+{
+    public class Calculator
+    {
+        public double Add(double num1, double num2) => num1 + num2;
+        public double Subtract(double num1, double num2) => num1 - num2;
+        public double Multiply(double num1, double num2) => num1 * num2;
+        public double Divide(double num1, double num2) => num1 / num2;
+    }
+}
