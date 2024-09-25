626d9deafd0f8b26063ed16639082281bdfa19b3
chuwilliamson <chuwilliamson@gmail.com>
2024-09-25 06:04:17 -0500

    start adding keyboard input, currently the focus isn't being handled correctly for the operator

--- WpfCalculator/MainWindow.xaml.cs
+++ WpfCalculator/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Input;
+using WpfCalculator.ViewModel;
 
 
 namespace WpfCalculator
@@ -14,5 +16,41 @@ namespace WpfCalculator
             // Set the DataContext to the ViewModel
             DataContext = new ViewModel.CalculatorViewModel();
         }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            var viewModel = DataContext as CalculatorViewModel;
+            if (viewModel == null) return;
+
+            // Detect if the user pressed an operator key
+            if (e.Key == Key.Add || e.Key == Key.OemPlus)
+            {
+                viewModel.OperationCommand.Execute("Add");
+                Number2TextBox.Focus(); // Move focus to Number2 after selecting operator
+            }
+            else if (e.Key == Key.Subtract || e.Key == Key.OemMinus)
+            {
+                viewModel.OperationCommand.Execute("Subtract");
+                Number2TextBox.Focus();
+            }
+            else if (e.Key == Key.Multiply)
+            {
+                viewModel.OperationCommand.Execute("Multiply");
+                Number2TextBox.Focus();
+            }
+            else if (e.Key == Key.Divide || e.Key == Key.OemQuestion)
+            {
+                viewModel.OperationCommand.Execute("Divide");
+                Number2TextBox.Focus();
+            }
+
+            // Check if Enter is pressed to calculate the result
+            if (e.Key == Key.Enter)
+            {
+                viewModel.EqualsCommand.Execute(null);
+                Number1TextBox.Focus(); // Reset focus to Number1 after calculation
+            }
+        }
+
     }
 }
